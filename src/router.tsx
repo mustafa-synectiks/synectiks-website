4be215439672444e3ea9523b9ab4941ae161888c
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2018-12-13 13:14:22 +0530

    changes made

--- src/router.tsx
+++ src/router.tsx
@@ -1,25 +1,24 @@
 import * as React from "react";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
-// import { App } from "./components/App";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { About } from "./components/About";
 import Background from "./components/Background/Background";
 import { Main } from "./components/Main/Main";
-// import Company from "./components/Pages/Company";
-import { Services, Company, Solutions } from "./components";
+
+import { Services, Company, Solutions, Resources } from "./components";
 
 export const AppRouter: React.StatelessComponent<{}> = () => {
   return (
     <BrowserRouter>
       <div>
         <Route component={Header} />
-        <Background />
-        <Main />
         <Switch>
+          <Route exact path="/" component={Main} />
           <Route exact path="/company" component={Company} />
-          <Route path="/services" component={Services} />
-          <Route path="/solutions" component={Solutions} />
+          <Route exact path="/services" component={Services} />
+          <Route exact path="/solutions" component={Solutions} />
+          <Route exact path="/resources" component={Resources} />
         </Switch>
         <Route component={Footer} />
       </div>
